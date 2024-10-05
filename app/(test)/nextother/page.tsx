0622830f59e0bed5fffeb8d935678bce8431b0d4
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-05 19:11:10 +0530

    exploring more in folder structure

---
+++ app/(test)/nextother/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nextOther = () => {
+  return (
+    <div>
+      this is nextOther;
+    </div>
+  )
+}
+
+export default nextOther
---
+++ app/(test)/other/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const otherPage = () => {
+  return (
+    <div>
+      this is other page;
+    </div>
+  )
+}
+
+export default otherPage
