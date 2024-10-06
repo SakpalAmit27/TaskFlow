e5eb26b29743a3a79ccb0315cde78767d7030a39
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-06 12:48:29 +0530

    implemented head

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,15 @@
+import { Medal } from 'lucide-react';
 import React from 'react'
 
 const marketingPage = () => {
   return (
     <div className='flex items-center justify-center flex-col'>
-      this is marketing page
+      <div className='flex items-center justify-center flex-col'>
+        <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
+            <Medal className='h-6 mr-2 w-6'/>
+            best task management
+        </div>
+      </div>
     </div>
   )
 }
