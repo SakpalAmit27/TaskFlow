129cd8e4e76da5c49b1bfd8864d34507855fec08
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-06 12:40:24 +0530

    some styling in marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const marketingPage = () => {
   return (
-    <div>
+    <div className='flex items-center justify-center flex-col'>
       this is marketing page
     </div>
   )
