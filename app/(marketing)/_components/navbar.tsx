e28a3780431dc1296818a5a0ff3209c70305ac68
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-16 11:59:50 +0530

    some changes made with logo along with import fixed for logo and implemented

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,7 +1,13 @@
+import { Logo } from "@/components/logo"
 export const Navbar = () => {
 
     return (
-        <div className="fixed top-0 w-full h-14 px-4 bg-white border-b flex items-center shadow-sm">NAV</div>
+        <div className="fixed top-0 w-full h-14 px-4 bg-white border-b flex items-center shadow-sm">
+            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+                <Logo/>
+            </div>
+        </div>
     )
 }
 
+ 
--- components/logo.tsx
+++ components/logo.tsx
@@ -2,17 +2,17 @@ import Link from "next/link"
 import Image from "next/image"
 import { cn } from "@/lib/utils"
 import localFont from "next/font/local"
-
+import logo from "@/app/fonts/logo.svg"
 
 const headingFont = localFont({
-     src:"../fonts/font.woff2",
+     src:"../app/fonts/font.woff2"
 })
 
 export const Logo = () => {
     return(
         <Link href="/">
             <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-                <Image src='../app/taskflow.svg' alt="logo" width={30} height={30}/>
+                <Image  src={logo}alt="logo" width={40} height={40}/>
                 <p className={cn("text-lg text-neutral-700 pb-1",headingFont.className)}>TaskFlow</p>
             </div>
         </Link>
